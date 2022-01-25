91c7a4ce86932caac16b5c8aa8f15d476903fa05
Netanel Haber <netanel@cardlatch.com>
2022-01-25 23:13:08 +0200

    original and decoded produce same value

--- huffman_code/get_file.py
+++
@@ -1,6 +0,0 @@
-from os import path
-
-location = path.dirname(path.abspath(__file__))
-def get_file(): 
-    with open(path.join(location, "raw.html")) as f:
-        return f.read() 
--- huffman_code/main.py
+++ huffman_code/main.py
@@ -1,7 +1,7 @@
 from heapq import heapify, heappop, heappush
 from collections import Counter
-from os import path
-from .get_file import get_file, location
+from turtle import right
+from .files import get_file, write_bin_to_file, read_bin_from_file
 from .node import Node
 
 def heap_from_counter(counter:Counter):
@@ -29,17 +29,43 @@ def code_from_tree(tree):
     return char_to_str_repr
 
 def encode(text, code):
-    joined = "".join([code[c] for c in text])
-    padding_length = len(joined)%8
+    joined = "".join(code[c] for c in text)
+    padding_length = 8 - (len(joined)%8)
     joined += "0"*(padding_length)
     barr = bytearray()
-    window = 0
-    paddedLength = len(joined)
-    while window<paddedLength:
-        barr.append(int(joined[window:window+8],base=2))
-        window+=8
+    idx = 0
+    padded_length = len(joined)
+    while idx<padded_length:
+        str_byte = joined[idx:idx+8]
+        barr.append(bin_str_to_int(str_byte))
+        idx+=8
     barr.append(padding_length)
     return barr
+
+
+def bin_str_to_int(str):
+    return int(str,base=2)
+
+def raw_bin(b):
+    return bin(b)[2:].rjust(8,"0")
+
+def hydrate_text(byte_arr):
+    string = "".join(raw_bin(b) for b in byte_arr[:-2])
+    last_padding = bin_str_to_int(raw_bin(byte_arr[-1]))
+    last = raw_bin(byte_arr[-2])[:-last_padding]
+    return string + last
+
+def decode(byte_arr:bytes, tree:Node):
+    text = ""
+    bin_text = hydrate_text(byte_arr)
+    node = tree
+    for bit in bin_text:
+        node = node.left if bit == "0" else node.right
+        if node.is_leaf():
+            text += node.char
+            node = tree
+    return text
+
 
 if __name__ == '__main__':
     text = get_file()
@@ -48,8 +74,11 @@ if __name__ == '__main__':
     tree = tree_from_heap(heap)
     code = code_from_tree(tree)
     encoded = encode(text, code)
-    with open(path.join(location,'out.bin'), 'wb') as f:
-        f.write(encoded)
+    write_bin_to_file(encoded)
+    bin_text = read_bin_from_file()
+    decoded = decode(bin_text, tree)
+    assert decoded == text
+
     
 
 
