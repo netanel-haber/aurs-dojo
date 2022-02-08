8ef36fb85205340216cf93fbdeb7e9a657b578f1
Netanel Haber <netanel@cardlatch.com>
2022-02-09 00:42:05 +0200

    main->append_print

--- pyc/append_print.py
+++ pyc/append_print.py
@@ -14,22 +14,22 @@ fn_consts = fn_code_obj.co_consts
 fn_names = fn_code_obj.co_names
 fn_code = fn_code_obj.co_code
 
-co_code = immutable_bytestring_splice(
-    fn_code,
-    -2,
-    op_byte("LOAD_GLOBAL"),
-    to_byte(len(fn_names)),
-    op_byte("LOAD_CONST"),
-    to_byte(len(fn_consts)),
-    op_byte("CALL_FUNCTION"),
-    to_byte(1),
-    op_byte("POP_TOP", num_bytes=2),
-)
-
 augemented = augment_code_object(
-    fn_code_obj, co_code, fn_consts + ("lol",), fn_names + ("print",)
+    fn_code_obj,
+    immutable_bytestring_splice(
+        fn_code,
+        -2,
+        op_byte("LOAD_GLOBAL"),
+        to_byte(len(fn_names)),
+        op_byte("LOAD_CONST"),
+        to_byte(len(fn_consts)),
+        op_byte("CALL_FUNCTION"),
+        to_byte(1),
+        op_byte("POP_TOP", num_bytes=2),
+    ),
+    fn_consts + ("lol",),
+    fn_names + ("print",),
 )
 
 dis(augemented)
-print(co_code)
 eval(augemented)
